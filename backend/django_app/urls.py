9abd8e1d6f2cd36d697d1cb99b2501e7889acace
Akshath <akshath.chowdhary@gmail.com>
2025-03-03 03:19:27 +0530

    Advanced Tasks Week-1

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse, JsonResponse
+import re
+
+def special_char(s):
+    regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+    return bool(regex.search(s))
 
 # def hello_world(request):
 #     return HttpResponse("Hello, world! This is our interneers-lab Django server.")
 
 def hello_name(request):
+    # Get 'name' and 'age' from the query string
     name=request.GET.get("name", "World")
-    return JsonResponse({"message": f"Hell0, {name}!"}) 
+    age=request.GET.get("age", None)
+    if special_char(name):
+        return JsonResponse({"error": "Invalid name. Name can't contain special characters"})
+    if not age:
+        return JsonResponse({"message": f"Hell0, {name}! Your age is unknown"}) 
+    elif int(age)<0:
+        return JsonResponse({"error": "Invalid age. Age can't be -ve"})
+    
+    return JsonResponse({"message": f"Hell0, {name}! Your age is {age}"}) 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
